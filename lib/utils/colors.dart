84f0674ae9a0eedae990ec03d106a4337f591d20
mehmet-tur <117525660+mehmet-tur@users.noreply.github.com>
2024-05-25 14:23:25 +0300

    Delete lib directory

--- lib/utils/colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const blackColor = Colors.black;
-const iconColor = Colors.black;
-const whiteColor = Colors.white;
-const blueColor = Colors.blue;
--- lib/widgets/app_style.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-appStyle(Color color, FontWeight fw, double size) {
-  return TextStyle(color: color, fontWeight: fw, fontSize: size);
-}
